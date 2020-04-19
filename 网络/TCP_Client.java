92a66948b3c30c994b2727d85f29d1cd41ab35e7
zj <1260543792@qq.com>
2020-04-19 17:34:33 +0800

    大幅度发

--- 网络/TCP_Client.java
+++ 网络/TCP_Client.java
@@ -5,13 +5,12 @@ import java.io.*;
 import java.net.*;
 
 public class TCP_Client {
-    public static void main(String[] args) throws IOException {
-        String request = "你好世界";
-        if (args.length > 0) {
-            request = args[0];
-        }
-
+    public static void main(String[] args) throws IOException, InterruptedException {
         Socket socket = new Socket("127.0.0.1", 9898);
+
+        Scanner console = new Scanner(System.in);
+        System.out.print("请输入请求> ");
+        String request = console.nextLine();
 
         OutputStream os = socket.getOutputStream();
         PrintWriter writer = new PrintWriter(
@@ -23,7 +22,7 @@ public class TCP_Client {
         InputStream is = socket.getInputStream();
         Scanner scanner = new Scanner(is, "UTF-8");
 
-        String response = scanner.nextLine();
+        String response = scanner.nextLine();   // 对象没有响应，就一直等
         System.out.println(response);
 
         socket.close();
